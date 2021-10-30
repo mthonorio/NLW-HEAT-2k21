d5f90ee8112067cf206279478a0d3acd984ef804
MtHonorio <matheushonorio@eng.ci.ufpb.br>
2021-10-30 15:59:47 -0300

    Finish Elixir Words Cloud

---
+++ elixir_tags/lib/elixir_tags/messages/create.ex
@@ -0,0 +1,13 @@
+defmodule ElixirTags.Messages.Create do
+  alias ElixirTags.{Message, Repo}
+
+  def call(params) do
+    params
+    |> Message.changeset()
+    |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %Message{}} = result), do: result
+  defp handle_insert({:error, result}), do: {:error, %{result: result, status: :bad_request}}
+end
---
+++ elixir_tags/lib/elixir_tags/messages/get.ex
@@ -0,0 +1,13 @@
+defmodule ElixirTags.Messages.Get do
+  import Ecto.Query
+
+  alias ElixirTags.{Message, Repo}
+
+  def today_messages do
+    today = Date.utc_today()
+
+    query = from message in Message, where: type(message.inserted_at, :date) == ^today
+
+    Repo.all(query)
+  end
+end
